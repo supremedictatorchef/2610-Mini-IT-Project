cfbe0adcda004b1eae234b64086457e4701c95d6
loozh <loozhiheng@gmail.com>
2026-06-06 20:46:04 +0800

    middleware connection for authorization in boostrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+        'club.management' => \App\Http\Middleware\CheckClubManagement::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
